43f29821478ef1d92d124c79c794f606ee102806
navyakalale <Navya613@gmail.com>
2015-09-29 11:41:46 -0400

    Update HelloWorld.java

--- app/src/main/java/com/navyakalale/helloworld/HelloWorld.java
+++ app/src/main/java/com/navyakalale/helloworld/HelloWorld.java
@@ -4,34 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelloWorld extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
-    }
+   //private int numtest = 0;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hello_world, menu);
-        return true;
-    }
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_hello_world);
+   }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+   public void buttonOnClick(View v) {
+       EditText e = (EditText) findViewById(R.id.editText);
+       TextView t = (TextView) findViewById(R.id.textView2);
+       String s = e.getText().toString();
+       t.setText("Hello " + s + "!");
+   }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+   @Override
 
-        return super.onOptionsItemSelected(item);
-    }
+   public boolean onCreateOptionsMenu(Menu menu) {
+       // Inflate the menu; this adds items to the action bar if it is present.
+       getMenuInflater().inflate(R.menu.menu_hello_world, menu);
+       return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+       // Handle action bar item clicks here. The action bar will
+       // automatically handle clicks on the Home/Up button, so long
+       // as you specify a parent activity in AndroidManifest.xml.
+       int id = item.getItemId();
+
+       //noinspection SimplifiableIfStatement
+       if (id == R.id.action_settings) {
+           return true;
+       }
+
+       return super.onOptionsItemSelected(item);
+   }
 }
